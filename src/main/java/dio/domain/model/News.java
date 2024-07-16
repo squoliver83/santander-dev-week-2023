3585caaae8e0e60211e10cfe252745901112d30c
Samuel Quaresma <squoliver@gmail.com>
2024-07-16 15:42:55 -0300

    Implementação da camada de domínio

---
+++ src/main/java/dio/domain/model/News.java
@@ -0,0 +1,7 @@
+package dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+}
---
+++ src/main/java/dio/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dio.domain.repository;
+
+import dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
